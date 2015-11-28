9a37fa5d413c0b6229f85ceb1f9e36a8e001020e
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2015-11-28 13:12:47 +0100

    added closure compilation ability + reload using live-reload

--- ui/src/Lib.hs
+++ ui/src/Lib.hs
@@ -10,4 +10,4 @@ import GHCJS.DOM
 foreign import javascript unsafe "window.alert($1)" js_alert :: JSString -> IO ()
 
 someFunc :: IO ()
-someFunc = js_alert "Hello from GHCJS!"
+someFunc = js_alert "Hello from GHCJS! ANOTHER TIME OK?"
