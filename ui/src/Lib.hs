9929f709fa61a92a10cb0950c45add6e8a796429
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2015-11-28 15:57:49 +0100

    just playing a little

--- ui/src/Lib.hs
+++ ui/src/Lib.hs
@@ -7,7 +7,23 @@ import GHCJS.Foreign ()
 import GHCJS.Types
 import GHCJS.DOM
 
+import GHCJS.DOM.Document
+import GHCJS.DOM.Node (appendChild)
+
 foreign import javascript unsafe "window.alert($1)" js_alert :: JSString -> IO ()
+foreign import javascript unsafe "document.getElementById($1).appendChild(document.createTextNode($2))" js_doc_append :: JSString -> JSString -> IO ()
 
 someFunc :: IO ()
-someFunc = js_alert "Hello from GHCJS! ANOTHER TIME OK?"
+someFunc = do
+    js_doc_append "dev" ": Hello from GHCJS FFI!"
+    m_doc <- currentDocument
+    case m_doc of
+         Nothing -> return ()
+         Just doc -> do
+            m_devDiv <- getElementById doc ("dev" :: String)
+            case m_devDiv of
+                 Just devDiv -> do
+                     t <- createTextNode doc ("Hello from GHCJS Dom!" :: String)
+                     appendChild devDiv t
+                     return ()
+                 Nothing -> return ()
