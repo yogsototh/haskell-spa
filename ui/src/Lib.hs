02ea2676be89070016c220c87620cfb8dfa14cdc
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2015-11-29 11:45:59 +0100

    append to body

--- ui/src/Lib.hs
+++ ui/src/Lib.hs
@@ -10,20 +10,22 @@ import GHCJS.DOM
 import GHCJS.DOM.Document
 import GHCJS.DOM.Node (appendChild)
 
-foreign import javascript unsafe "window.alert($1)" js_alert :: JSString -> IO ()
-foreign import javascript unsafe "document.getElementById($1).appendChild(document.createTextNode($2))" js_doc_append :: JSString -> JSString -> IO ()
+-- foreign import javascript unsafe "window.alert($1)" js_alert :: JSString -> IO ()
+foreign import javascript unsafe "document.body.appendChild(document.createElement(\"div\").appendChild(document.createTextNode($1)))" js_doc_append :: JSString -> IO ()
+
+mbind :: IO (Maybe a) -> (a -> IO (Maybe b)) -> IO (Maybe b)
+mbind v f = v >>= maybe (return Nothing) f
+
+void :: IO a -> IO ()
+void f = f >> return ()
 
 someFunc :: IO ()
 someFunc = do
-    js_doc_append "dev" ": Hello from GHCJS FFI!"
-    m_doc <- currentDocument
-    case m_doc of
-         Nothing -> return ()
-         Just doc -> do
-            m_devDiv <- getElementById doc ("dev" :: String)
-            case m_devDiv of
-                 Just devDiv -> do
-                     t <- createTextNode doc ("Hello from GHCJS Dom!" :: String)
-                     appendChild devDiv t
-                     return ()
-                 Nothing -> return ()
+    js_doc_append "Hello from GHCJS FFI!"
+    void $ currentDocument `mbind` (\ doc ->
+            getBody doc `mbind` (\ body ->
+              createElement doc (Just "div" :: Maybe String) `mbind` (\divElem -> do
+                 t <- createTextNode doc ("Hello from GHCJS Dom!" :: String)
+                 _ <- appendChild divElem t
+                 _ <- appendChild body (Just divElem)
+                 return Nothing)))
